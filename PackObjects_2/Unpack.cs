1dd914ca8b8d78a78d9c6689ca98e7c3a75edf23
eduardsaakev <Eduard.Sakaiev@globalLogic.com>
2014-12-17 19:59:12 +0200

    Some changes in pack projects_2 + examples of unpack

---
+++ PackObjects_2/Unpack.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class MainClass
+{
+	public static void Main () 
+	{
+		Int32 х = 5;
+		Object о = х; // У п а ко в ка х : о у к а з ы в а е т на упакованный объ ект
+		//Int16 у = (Int16)о; // Распаковка . а з а тем при ведение т и п а not ok Invalidcastexception
+		Int16 z = (Int16)(Int32)о; // Распаковка . а з а тем при ведение т и п а ok
+	}
+}
+
+
+
+
+
+
+
+
