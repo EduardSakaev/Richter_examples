9b58249810d2af51fcc2998a94e9f722d6248a2b
eduardsaakev <Eduard.Sakaiev@globalLogic.com>
2014-12-18 17:35:28 +0200

    Dynamic type

---
+++ Dynamic_type/Dynamic_demo_1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class DynamicDemo {
+	public static void Main() {
+		for (Int32 demo = 0 ; demo < 2 ; demo++ ) {
+			dynamic arg = (demo == 0) ? (dynamic)5 : (dynamic)'A';
+			dynamic result = Plus (arg);
+			M (result);
+		}
+	}
+		private static dynamic Plus ( dynamic arg ) { return arg + arg; }
+		private static void M(Int32 n) { Console.WriteLine ( " M ( i nt32 ) : " + n ); }
+		private static void M(String s) { Console.WriteLine ( " M ( St r i ng ) : " + s );}
+}
+
---
+++ PackObjects/Change_fileds_in_packed_value_type_by_interfaces.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+// И н терфейс . оnредел я ющ ий метод Cha nge
+internal interface IChangeBoxedPoint {
+	void Change (Int32 х , Int32 у) ;
+}
+
+//Poi nt значимый тиn
+internal struct Point : IChangeBoxedPoint
+{
+	private Int32 m_x, m_y;
+
+	public Point (Int32 x, Int32 y) {
+		m_x = x ;
+		m_y = y ;
+	}
+
+	public void Change (Int32 х, Int32 у) {
+		m_x = х ;
+		m_y = у ;
+	}
+
+	public override String ToString () {
+		return String.Format ("({0},{1})", m_x, m_y);
+	}
+}
+
+public sealed class Program {
+	public static void Main () {
+	Point p = new Point (1 , 1);
+		Console.WriteLine ( p);  // p пакуется
+		p.Change (2, 2);        //
+		Console.WriteLine (p);   // не пакуется
+		Object o = p;            // пакуется р упаковывается в третий разо ссылается н а упакованный объект типа Poi nt.
+		Console.WriteLine (o); 
+		/* Далее р упаковывается в третий раз
+		о ссылается н а упакованный объект
+		типа Poi nt. При третьем обращении к W r i tel i ne снова выводится ( 2, 2 ) , что
+		опять вполне ожидаемо. И наконец, я обращаюсь к методу Cha nge для изме­
+		нения полей в упакованном объекте типа Poi nt. Между тем Object (тип пере­
+		менной о) ничего не •знает• о методе Cha nge, так что сначала нужно привести
+		о к Poi nt. При таком приведении типа о распаковывается, и поля упакованного
+		объекта типа Poi nt копируются во временный объект типа Poi nt в стеке потока.
+		Поля m_х и m_у этого временного объекта устанавливаются равными 3, но это
+		обращение к Cha nge не влияет на упакованный объект Poi nt. При обращении
+		к Wri tel i ne снова выводится ( 2, 2 ) . Для многих разработчиков это оказывается
+		иео�даииоапью.*/
+
+		((Point)o).Change(3, 3); //will still print 2,2 :(
+		Console.WriteLine (o);
+
+		// р упаковывается . у п а к о в а н н ы й объ ект и з м ен я ется и освобождается
+		((IChangeBoxedPoint) p ).Change ( 4, 4 );
+		Console.WriteLine(p);
+
+		// Упа кованный объ ект и з м е н я ется и выводится
+		((IChangeBoxedPoint)o).Change ( 5 , 5 ) ;
+		Console.WriteLine(o);
+	}
+}
+
+/*There is the way to fix the problem:
+ * Можно пофиксить проблему, применив интерфейс IChangeBoxedPoint */
+
+
+
+
